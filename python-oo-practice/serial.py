f44095c47e06a49de436d6ff6481bbbc3792e251
Personal <Home@Joses-MacBook-Pro.local>
2021-03-18 15:43:08 -0700

    Python OO Exercises

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -19,4 +19,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self,start):
+        self.start = start
+
+    def generate(self):
+        ''' Returns the next incremental value initially passed to the class and adds 1 to said value'''
+        print(self.start)
+        self.start += 1
+    
+    def reset(self):
+        '''Resets the current value back to the initial value'''
+        self.start = 100
+
+
 
--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,36 @@
 """Word Finder: finds random words from a dictionary."""
+from random import choice
+
 
 
 class WordFinder:
-    ...
+    '''creates a class instance of wordfinder with a method for retuning one random word
+    from the text file that was specified when the class was defined'''
+
+    def __init__(self, path):
+        file = open(path)
+        self.words = self.parse(file)
+        print(f'{len(self.words)} words read')
+ 
+    def  parse(self, file):
+        return [w.strip() for w in file]
+
+    def random(self):
+        '''
+        Method that returns a random word from the list of words.
+        '''
+        return choice(self.words)
+   
+
+class RandomWordFinder(WordFinder):
+    def parse(self, file):
+        '''
+        Method that returns a random word from a list of words but omits blank lines
+        or words that begin with a special character.        
+        '''
+        return [w.strip() for w in file if w.strip() and not w.startswith('#')]
+
+    
+
+
+    
